1b195ae3556b0361b7fe120bc6be5f5a0f8bc708
Douglas Einstein <de.or.de@hotmail.com>
2018-03-05 04:40:06 -0300

    Fazendo a busca com inner join

--- app/controllers/TransactionController.scala
+++ app/controllers/TransactionController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject.{Inject, Singleton}
 
 import controllers.ResponseWriters.transactionWrites
+import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
 import services.{TransactionFilters, TransactionRepository}
@@ -14,10 +15,13 @@ class TransactionController @Inject()(cc: ControllerComponents, transactionRepos
 
   def searchTransaction(referenceCode: Option[String], bankNumber: Option[String], establishment: Option[String], bank: Option[String], status: Option[String], amount: Option[Int]) = Action.async {
     val transactionFilters = TransactionFilters(referenceCode, bankNumber, establishment, bank, status, amount)
+    Logger.info(s"action=endpoint-request uri=/transacions method=GET $transactionFilters")
     transactionRepository.findTransactionsByReference(transactionFilters)
-      .map(seq =>
-        if(seq.isEmpty) NotFound else Ok(Json.toJson(seq))
-      )
+      .map { seq =>
+        val size = seq.size
+        Logger.info(s"action=endpoint-response uri=/transacions method=GET $transactionFilters $size transactions found")
+        if (seq.isEmpty) NotFound else Ok(Json.toJson(seq))
+      }
   }
 
 }
--- app/services/TransactionRepository.scala
+++ app/services/TransactionRepository.scala
@@ -30,50 +30,44 @@ class TransactionRepositoryImpl extends TransactionRepository {
     val filterBoletoByBankNumber = transactionFilters.bankNumber.map(bn => boletos.filter(_.bankNumber === bn)).getOrElse(boletos)
     val filterBoleto = transactionFilters.amount.map(a => filterBoletoByBankNumber.filter(_.amount === a)).getOrElse(filterBoletoByBankNumber)
     val filterBank = transactionFilters.bank.map(s => banks.filter(_.code === s)).getOrElse(banks)
-    val filterNormalizedStatus = transactionFilters.normalizedStatus.map(n => normalizedStatus.filter(_.messsage === n)).getOrElse(normalizedStatus)
+    val filterNormalizedStatus = transactionFilters.normalizedStatus.map(n => normalizedStatus.filter(_.code === n)).getOrElse(normalizedStatus)
 
     val transactionQuery = for {
-      (((((((t, e), bo), ba), n), c), ci), p) <- filterTransaction join filterEstablishment on (_.establishmentId === _.id) join filterBoleto on (_._1.boletoId === _.id) joinLeft filterBank on (_._1._1.bankId === _.id) joinLeft filterNormalizedStatus on (_._1._1._1.normalizedStatusId === _.id) joinLeft cascadeLogs on (_._1._1._1._1.cascadeLogId === _.id) joinLeft cascadeLogItems on (_._2.map(_.id) === _.cascadeLogId) joinLeft payments on (_._1._1._1._1._1._1.id === _.transactionId)
+      (((((((t, e), bo), ba), n), c), ci), p) <- filterTransaction join filterEstablishment on (_.establishmentId === _.id) join filterBoleto on (_._1.boletoId === _.id) join filterBank on (_._1._1.bankId === _.id) join filterNormalizedStatus on (_._1._1._1.normalizedStatusId === _.id) joinLeft cascadeLogs on (_._1._1._1._1.cascadeLogId === _.id) joinLeft cascadeLogItems on (_._2.map(_.id) === _.cascadeLogId) joinLeft payments on (_._1._1._1._1._1._1.id === _.transactionId)
     } yield (t, e, bo, ba, n, c, ci, p)
 
-    val result = transactionQuery.take(100).result
-
-    Logger.info(result.statements.toString())
+    val result = transactionQuery.sortBy(t => t._1.id.desc).take(30).result
 
     db.run(result)
       .map(mapTableRows(_))
-      .map(seq => seq.filter(shouldFilterTuple(_)))
       .map {
         _.map { tuple =>
           val establishment = new Establishment(tuple._2)
           val boleto = new Boleto(tuple._3)
-          val optionBank = tuple._4.map(new Bank(_))
-          val optionNormalizedStatus = tuple._5.map(new NormalizedStatus(_))
+          val bank = new Bank(tuple._4)
+          val normalizedStatus = new NormalizedStatus(tuple._5)
           val cascadeLog = tuple._6.map(new CascadeLog(_, tuple._7.map(new CascadeLogItem(_))))
           val paymentsList = tuple._8.map(new Payment(_))
 
-          new Transaction(tuple._1, establishment, boleto, optionBank, optionNormalizedStatus, cascadeLog, paymentsList)
+          new Transaction(tuple._1, establishment, boleto, Option(bank), Option(normalizedStatus), cascadeLog, paymentsList)
         }
       }
   }
 
-  private def mapTableRows(seq: Seq[(TransactionDB, EstablishmentDB, BoletoDB, Option[BankDB], Option[NormalizedStatusDB], Option[CascadeLogDB], Option[CascadeLogItemDB], Option[PaymentDB])]): Seq[(TransactionDB, EstablishmentDB, BoletoDB, Option[BankDB], Option[NormalizedStatusDB], Option[CascadeLogDB], Seq[CascadeLogItemDB], Seq[PaymentDB])] = {
+  private def mapTableRows(seq: Seq[(TransactionDB, EstablishmentDB, BoletoDB, BankDB, NormalizedStatusDB, Option[CascadeLogDB], Option[CascadeLogItemDB], Option[PaymentDB])]): Seq[(TransactionDB, EstablishmentDB, BoletoDB, BankDB, NormalizedStatusDB, Option[CascadeLogDB], Seq[CascadeLogItemDB], Seq[PaymentDB])] = {
     if (seq.isEmpty) {
       Seq.empty
     } else {
       val transactionsTuplesMap = seq.groupBy(_._1.id)
 
-      transactionsTuplesMap.values.map { v =>
-        val head = v.head
-        val seqCascadeLogItem = seq.map(_._7).filter(_.isDefined).map(_.get).distinct
-        val seqPayment = seq.map(_._8).filter(_.isDefined).map(_.get).distinct
+      transactionsTuplesMap.keys.map { k =>
+        val tuples = transactionsTuplesMap(k)
+        val head = tuples.head
+        val seqCascadeLogItem = tuples.map(_._7).filter(_.isDefined).map(_.get).distinct
+        val seqPayment = tuples.map(_._8).filter(_.isDefined).map(_.get).distinct
 
         (head._1, head._2, head._3, head._4, head._5, head._6, seqCascadeLogItem, seqPayment)
       }.toIndexedSeq
     }
-  }
-
-  private def shouldFilterTuple(tuple: (TransactionDB, EstablishmentDB, BoletoDB, Option[BankDB], Option[NormalizedStatusDB], Option[CascadeLogDB], Seq[CascadeLogItemDB], Seq[PaymentDB])): Boolean = {
-    tuple._4.isDefined
   }
 }
